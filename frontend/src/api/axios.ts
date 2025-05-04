5f97bad90a0d912ea8cc152074839a87f042b569
Jonathan <jonathanramallo1996@gmail.com>
2025-05-05 01:06:35 +0200

    pequeña correccion de bugs

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // Ajusta si cambias de puerto/backend
+ baseURL: `${window.location.protocol}//${window.location.hostname}:8000/api/`, // Ajusta si cambias de puerto/backend
   headers: {
     'Content-Type': 'application/json',
   },
